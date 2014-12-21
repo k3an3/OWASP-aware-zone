1737c9964ba721adbe1978c2921fdc7a07f9fabe
Keane O'Kelley <korockinout13@gmail.com>
2014-12-21 15:12:25 -0600

    Add cookie and fix title

--- xss/index.php
+++ xss/index.php
@@ -1,10 +1,12 @@
+<?php
+setcookie("Secret", "Your super secret session ID=5d440ff3c129c01993dc2b", time() + 3600);
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
 <link rel="shortcut icon" href="mat.ico">
-<title>Burpsuite Test Site</title>
+<title>XSS Demo</title>
 </head>
-
 <body>
 <h1>Very Flashy and Complicated Website 2</h1>
 <p>Welcome to my site. This site has extreme security. My site can't be hacked.</p>
@@ -15,7 +17,7 @@ Leave me a comment: <input type="text" name="cool">
 <input type="submit" value="submit">
 </form>
 <br>
-<?php 
+<?php
 if (isset($_POST['cool']) && !isset($_POST['button'])) {
 	echo "<h1>Thanks for the compliment!</h1><br/>";
 	echo "You said: "; 
