76868cc00ada82894e6471380beed6d541c04a29
Keane O'Kelley <korockinout13@gmail.com>
2014-12-21 21:51:08 -0600

    Fix SQL field list

--- sql/newaccount.php
+++ sql/newaccount.php
@@ -11,7 +11,7 @@ if($_POST['user'] === null || $_POST['pass'] === null) { ?>
 <p>Username:</p>
 <input type="text" name="user">
 <p><br/>Password:</p>
-<input type="text" name="pass">
+<input type="password" name="pass">
 <input type="submit" value="Submit">
 </form>
 
@@ -23,7 +23,7 @@ else {
     if (!$conn) 
 	die("Failed to connect to MySQL: " . mysqli_error($conn));
     mysqli_select_db($conn, 'sqldemo');
-    $result = mysqli_query($conn, "INSERT INTO Users (Name, Password, Secret) VALUES($username, $password, 'None')");
+    $result = mysqli_query($conn, "INSERT INTO Users (Name, Password, Secret) VALUES('$username', '$password', 'None')");
     if(!$result)
 	die("Failed to add user. MySQL Error: " . mysqli_error($conn));
     echo 'User added successfully! You may now proceed to <a href="login.php">login</a>.';
