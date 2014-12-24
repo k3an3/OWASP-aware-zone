be942ba39ee9d50aa596b5f44c4de3c22b13862d
Keane O'Kelley <korockinout13@gmail.com>
2014-12-24 12:27:41 -0600

    Disallow creating duplicate usernames

--- sql/newaccount.php
+++ sql/newaccount.php
@@ -5,8 +5,6 @@
 </head>
 <body>
 <h1>Create an Account</h1>
-<?php
-if($_POST['user'] === null || $_POST['pass'] === null) { ?>
 <form method="post">
 <p>Username:</p>
 <input type="text" name="user">
@@ -15,14 +13,18 @@ if($_POST['user'] === null || $_POST['pass'] === null) { ?>
 <input type="submit" value="Submit">
 </form>
 
-<?php }
-else {
+<?php 
+if($_POST['user'] !== null || $_POST['pass'] !== null) {
     $username = $_POST['user'];
     $password = $_POST['pass'];
     $conn = mysqli_connect('localhost', 'username', 'password');
     if (!$conn) 
 	die("Failed to connect to MySQL: " . mysqli_error($conn));
     mysqli_select_db($conn, 'sqldemo');
+    $result = mysqli_query($conn, "Select * FROM Users WHERE Name = '$username'");
+    $data = mysqli_fetch_assoc($result);
+    if(mysqli_num_rows($result) != 0) 
+	 die("<br/>A user with that username already exists. Try again.");
     $date = date('l F jS Y');
     $result = mysqli_query($conn, "INSERT INTO Users (Name, Password, Secret, numposts, datejoined) VALUES('$username', '$password', 'None', 0, '$date')");
     if(!$result)
