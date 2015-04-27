166cf0c2ad4ce602e4cae33fedfbfcdc957cb2b1
Keane O'Kelley <korockinout13@gmail.com>
2015-04-27 15:02:04 -0500

    Fix overflow, again. Make token gen even weaker in the process.

--- sql/login.php
+++ sql/login.php
@@ -14,21 +14,21 @@
 <input type="submit" value="Submit">
 </form>
 
-<?php 
+<?php
 /* Grab login information from POST. Check to see if there is a matching username and password in the database. If so, generate a unique sessionID and save it in the Sessions table.
 Otherwise, let the user know they can't log in. */
 if($_POST['user'] !== null || $_POST['pass'] !== null) {
     $username = $_POST['user'];
     $password = $_POST['pass'];
     $conn = mysqli_connect('localhost', 'username', 'password');
-    if (!$conn) 
+    if (!$conn)
 	die("Failed to connect to MySQL: " . mysqli_error($conn));
     mysqli_select_db($conn, 'sqldemo');
     $result = mysqli_query($conn, "SELECT * FROM Users WHERE Name = '$username' AND password = '$password'");
     if(mysqli_num_rows($result) == 0)
 	die("<br/>Could not find a user with that username and password combination.");
      $data = mysqli_fetch_assoc($result);
-     $session = intval(str_replace('.', '', $_SERVER['REMOTE_ADDR'])) / 10000000 * time() * 17 + $data['ID']; 
+     $session = intval(str_replace('.', '', $_SERVER['REMOTE_ADDR'])) / 10 * time() * 17 + $data['ID'];
      $result = mysqli_query($conn, "INSERT INTO Sessions (username, sessionID) values(\"$username\", \"$session\")");
      echo "Logged in as " . $username . ". Redirecting...";
      echo '<meta http-equiv="refresh" content="0;URL=index.php?session=' . $session . '">';
