b534b1da2e7097b64d5e516d99251081524af0bb
Keane O'Kelley <korockinout13@gmail.com>
2016-03-22 14:48:32 -0500

    Strict rules

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+/sbin/iptables -A OUTPUT -o lo -j ACCEPT
+/sbin/iptables -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+/sbin/iptables -A OUTPUT -j REJECT
 service mysql start
 echo "CREATE DATABASE sqldemo; GRANT ALL ON sqldemo.* to 'username'@'localhost' IDENTIFIED BY 'password';" | mysql
 /usr/bin/php /var/www/demos/sql/index.php reset=true
