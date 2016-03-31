9f6afd21d2cc09684478ce3c85b546256701510a
Keane O'Kelley <korockinout13@gmail.com>
2016-03-31 17:41:44 -0500

    Finally fix regex

--- app/cmd/index.php
+++ app/cmd/index.php
@@ -6,23 +6,25 @@
 
 <h1>Free Online Ping Service</h1>
 
-<form method="post" action="">
+<form method="get" action="">
 <p>Enter address to ping:</p>
 <input type="text" name="address" placeholder="8.8.8.8">
 <input type="submit"/>
 </form>
 <br>
 <?php
-       if (ISSET($_POST['address'])) {
-           echo "Results:<br>";
-           if (preg_match("/^.*; ?(cat|ls|echo|uname|id|whoami) [^&|:;\\]*$/", $_POST['address'])) {
-               echo system("ping -c 4 " . $_POST['address']);
-           } else if (preg_match("/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/", $_POST['address'])) {
-               echo system("ping -c 4 " . $_POST['address']);
-           } else if (preg_match("/^.*; ?.*")){
-               echo "You're on the right track! However, a very limited set of commands is supported. Try `cat /etc/passwd`";
-           }
-       }
+if (ISSET($_REQUEST['address'])) {
+    echo "Results:<br>";
+    if (preg_match("/^.*; ?(ls|cat|echo|ping|grep|find|uname|id|whoami) ?[^|&;]*$/", $_REQUEST['address'])) {
+        echo system("ping -c 4 " . $_REQUEST['address']);
+    } else if (preg_match("/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/", $_REQUEST['address'])) {
+        echo system("ping -c 4 " . $_REQUEST['address']);
+    } else if (preg_match("/^.*; ?.*$/", $_REQUEST['address'])){
+        echo "You're on the right track! However, a very limited set of commands are supported. Try `cat /etc/passwd`";
+    } else {
+        echo "Something went horribly, horribly wrong.";
+    }
+}
 ?>
 <br>
 <br>
